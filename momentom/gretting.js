80d78044151ad5527efbcd262c425ad324de2dc8
Hajin Kim <enro302@naver.com>
2019-12-18 13:37:43 +0900

    ToDolist, 배경이미지 추가

--- momentom/gretting.js
+++ momentom/gretting.js
@@ -5,58 +5,29 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
 SHOWING_CN = "showing";
 
-// function saveName(text){
-//   localStorage.setItem(USER_LS, text);
-// }
-const saveName = text => {
-  localStorage.setItem(USER_LS, text)
-}; 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
 
-// function handleSubmit(event) {
-//   event.preventDefault(); 
-//   const currentValue = input.value;
-//   paintGreeting(currentValue); 
-//   saveName(currentValue);
-// }
-const handleSubmit = event => {
+function handleSubmit(event) {
   event.preventDefault(); 
   const currentValue = input.value;
   paintGreeting(currentValue); 
   saveName(currentValue);
 }
 
-// function askForName(){
-//   form.classList.add(SHOWING_CN);
-//   form.addEventListener("submit", handleSubmit);
-// }
-
-// const askForName -> 선언
-// const askForName = 1; -> 선언과 초기화 
-const askForName = () => {
+function askForName(){
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
-}
+} 
 
-// function paintGreeting(text){
-//   form.classList.remove(SHOWING_CN);
-//   greeting.classList.add(SHOWING_CN);
-//   greeting.innerText=`Hello ${text}`;
-// }
-const paintGreeting = text => {
+function paintGreeting(text){
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText=`Hello ${text}`;
-}  
+}
 
-// function loadName() {
-//   const currentUser = localStorage.getItem(USER_LS);
-//   if(currentUser === null){//유저가 없는 경우 
-//     askForName();  
-//   }else{ //유저가 있는 경우
-//     paintGreeting(currentUser);
-//   }
-// }
-const loadName = () => {
+function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if(currentUser === null){//유저가 없는 경우 
     askForName();  
@@ -65,10 +36,8 @@ const loadName = () => {
   }
 }
 
-// function init() {
-//   loadName();
-// }
-const init = () => {
+
+function init() {
   loadName();
 }
-
+init();
--- momentom/todo.js
+++ momentom/todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo){
+    return toDo.id !== parseInt(li.id);     
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
 
 function saveToDos(){
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,6 +29,7 @@ function paintToDo(text){
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerHTML = "X";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text
   li.appendChild(delBtn); 
   li.appendChild(span);
@@ -30,7 +43,7 @@ function paintToDo(text){
   saveToDos();
 }
 
-function handleSubmitTodo(event){
+function handleSubmit(event){
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
@@ -48,8 +61,8 @@ function loadToDos(){
   }
 } 
 
-function initTodo() {
+function init() {
   loadToDos();
-  toDoForm.addEventListener("submit", handleSubmitTodo)
+  toDoForm.addEventListener("submit", handleSubmit)
 }
-initTodo();
+init();
