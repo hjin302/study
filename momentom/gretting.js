b3be526950577da59fae029edef6050958f79b76
Hajin Kim <enro302@naver.com>
2019-12-27 10:36:24 +0900

    7번 문제 완료 !

--- momentom/gretting.js
+++ momentom/gretting.js
@@ -1,58 +1,58 @@
-const form = document.querySelector(".js-form"),
+const
+  form = document.querySelector(".js-form"),
   input = form.querySelector("input");
-  greeting = document.querySelector(".js-greetings");
+greeting = document.querySelector(".js-greetings");
+
 
 const USER_LS = "currentUser",
-SHOWING_CN = "showing";
+  SHOWING_CN = "showing";
 
-function saveName(text){
+function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
 
-function handleSubmit(event) {
-  event.preventDefault(); 
+function grHandleSubmit(event) {
+  event.preventDefault();
   const currentValue = input.value;
-  paintGreeting(currentValue); 
+  paintGreeting(currentValue);
   saveName(currentValue);
 }
 
-function askForName(){
+function askForName() {
   form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
-} 
-
-function paintGreeting(text){
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText=`Hello ${text} `;
-  const btn = document.createElement('button');
-  const cd = document.getElementsByClassName('greeting')
-  btn.textContent = '이름변경';
-  cd[0].append(btn);
-  btn.addEventListener("click", onClickCdBtn);
+  form.addEventListener("submit", grHandleSubmit);
 }
 
-function onClickCdBtn(event){
-  event.preventDefault(); 
-  const newName = document.createElement('input');
-  const btn = document.createElement('button');
-  const cd = document.getElementsByClassName('greeting')
-  btn.textContent = '변경하기';
-  cd[0].append(newName);
-  cd[0].append(btn);
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text} `;
+  const cdBtn = document.createElement('button');
+  const cd = document.getElementsByClassName('greeting')[0]
+  cdBtn.textContent = '변경';
+  cd.append(cdBtn);
+  cdBtn.addEventListener("click", onClickCdBtn);
+}
+
+function onClickCdBtn(event) {
+  event.preventDefault();
+  //greeting.setAttribute('style', 'display:none');
+  greeting.classList.remove(SHOWING_CN);
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", grHandleSubmit);
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
-  if(currentUser === null){//유저가 없는 경우 
-    askForName();  
-  }else{ //유저가 있는 경우
+  if (currentUser === null) { //유저가 없는 경우 
+    askForName();
+  } else { //유저가 있는 경우
     paintGreeting(currentUser);
   }
 }
 
-
 function init() {
   loadName();
 }
+
 init();
